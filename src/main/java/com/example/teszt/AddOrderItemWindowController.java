ff6532cce5e6cc09bff7853f1ab8d6cb7284774e
bombasztikus <180638283+bombasztikus@users.noreply.github.com>
2025-03-31 15:18:22 +0200

    wip: add order item

--- src/main/java/com/example/teszt/AddOrderItemWindowController.java
+++ src/main/java/com/example/teszt/AddOrderItemWindowController.java
@@ -1,16 +1,21 @@
 package com.example.teszt;
 
 import com.example.teszt.lib.*;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class AddOrderItemWindowController implements Initializable {
@@ -19,7 +24,16 @@ public class AddOrderItemWindowController implements Initializable {
     private ChoiceBox<Meal> mealSelection;
 
     @FXML
+    private Label mealSelectionRequiredLabel;
+
+    @FXML
     private TextField quantityField;
+
+    @FXML
+    private Label quantityRequiredLabel;
+
+    @FXML
+    private Button edit_orderitem;
 
     private HelloController mainController;
 
@@ -46,7 +60,7 @@ public class AddOrderItemWindowController implements Initializable {
             OrderItemRequest.create(quantity, mealSelection.getSelectionModel().getSelectedItem().id, selectedOrder.id);
             mainController.updateorderitems();
         } catch (Api_error e) {
-            showAPIExceptionModal(e.error);
+            showApiExceptionPopUp(e.error);
         }
 
         Stage stage = (Stage) mealSelection.getScene().getWindow();
@@ -56,13 +70,108 @@ public class AddOrderItemWindowController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         mealSelection.getItems().addAll(meals);
+        fieldValueManager();
+        buttonDisableManager();
+        requiredFieldManager();
     }
 
-    private void showAPIExceptionModal(String message) {
+    private void fieldValueManager() {
+        mealSelection.setItems(FXCollections.observableArrayList(meals)); // Ensure MealType is used, not Strings
+
+        mealSelection.setConverter(new StringConverter<Meal>() {
+
+            @Override
+            public String toString(Meal meal) {
+                return meal.toString();
+            }
+
+            @Override
+            public Meal fromString(String s) {
+                for (Meal m : meals) {
+                    if (Objects.equals(m.toString(), s)) {
+                        return m;
+                    }
+                }
+
+                return null;
+            }
+        });
+
+        quantityField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    quantityField.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+    }
+
+    private void buttonDisableManager() {
+        edit_orderitem.disableProperty().bind(Bindings.or(
+                quantityField.textProperty().isEmpty(),
+                mealSelection.valueProperty().isNull()
+        ));
+    }
+
+    private void requiredFieldManager() {
+        nameRequiredLabel.managedProperty().bind(nameRequiredLabel.visibleProperty());
+        nameRequiredLabel.visibleProperty().bind(nameField.textProperty().isEmpty());
+
+        typeRequiredLabel.managedProperty().bind(typeRequiredLabel.visibleProperty());
+        typeRequiredLabel.visibleProperty().bind(typeField.valueProperty().isNull());
+
+        List<TextField> fields = new ArrayList<>();
+        fields.add(nameField);
+        fields.add(caloriesField);
+        fields.add(priceField);
+
+        for (TextField field : fields) {
+            invalidFieldClassManager(field, field.getText());
+
+            field.textProperty().addListener((observable, oldValue, newValue) -> {
+                invalidFieldClassManager(field, newValue);
+            });
+        }
+
+        invalidChoiceClassManager(typeField, typeField.getValue());
+        typeField.selectionModelProperty().addListener((observable, oldValue, newValue) -> {
+            invalidChoiceClassManager(typeField, newValue.getSelectedItem());
+        });
+    }
+
+    private void invalidFieldClassManager(TextField field, String value) {
+        if (value != null && value.isEmpty()) {
+            if (!field.getStyleClass().contains("invalid")) {
+                field.getStyleClass().add("invalid");
+            }
+        } else {
+            field.getStyleClass().remove("invalid");
+        }
+
+        field.applyCss();
+        field.layout();
+    }
+
+    private <T> void invalidChoiceClassManager(ChoiceBox<T> choices, T value) {
+        if (value != null) {
+            if (!choices.getStyleClass().contains("invalid")) {
+                choices.getStyleClass().add("invalid");
+            }
+        } else {
+            choices.getStyleClass().remove("invalid");
+        }
+
+        choices.applyCss();
+        choices.layout();
+    }
+
+    private void showApiExceptionPopUp(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("A tétel nem adható hozzá");
-        alert.setHeaderText("Kérlek javítsd ki a hibát, majd próbáld újra.");
+        alert.setTitle("Probléma adódott");
         alert.setContentText(message);
+        alert.setHeaderText("Probléma adódott a termék hozzáadása közben");
         alert.showAndWait();
     }
 }
